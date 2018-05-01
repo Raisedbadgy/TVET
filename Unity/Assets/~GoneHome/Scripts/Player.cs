75f48274bd25c5cab446aa09c3c0b50cf43711df
Raisedbadge <james.pearce846@gmail.com>
2018-05-01 17:50:28 +1000

    Craet Visualstudio programming

---
+++ Unity/Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string massage = "Hello World";
+    public Rigidbody rigid;
+    float speed = 5;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rigid.AddForce(Vector3.forward * speed);
+        }
+            
+        
+
+    }
+}
